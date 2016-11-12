c584554348320b04b9f64f07269fd78b79d0aaba
Christine van den Vlekkert <xine.vdv@me.com>
2016-11-12 11:28:50 -0800

    testing maps

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+var mapOptions = {
+   center: new google.maps.LatLng(37.7831,-122.4039),
+   zoom: 12,
+   mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+
+new google.maps.Map(document.getElementById('map'), mapOptions);
+
+var markerOptions = {
+  position: new google.maps.LatLng(37.7831, -122.4039)
+};
+var marker = new google.maps.marker(markerOptions);
+marker.setMap(map);
