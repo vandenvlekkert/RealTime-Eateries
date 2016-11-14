2a0829e68082858cbbf39bd60b9707e62efb90a3
Christine van den Vlekkert <xine.vdv@me.com>
2016-11-14 10:47:55 -0800

    adding components to geolocation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,62 @@
-var mapOptions = {
-   center: new google.maps.LatLng(37.7831,-122.4039),
-   zoom: 12,
-   mapTypeId: google.maps.MapTypeId.ROADMAP
-};
+function writeAddressName(latLng) {
+        var geocoder = new google.maps.Geocoder();
+        geocoder.geocode({
+          "location": latLng
+        },
+        function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK)
+            document.getElementById("address").innerHTML = results[0].formatted_address;
+          else
+            document.getElementById("error").innerHTML += "Unable to retrieve your address" + "<br />";
+        });
+      }
 
-var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+      function geolocationSuccess(position) {
+        var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        // Write the formatted address
+        writeAddressName(userLatLng);
 
-var acOptions = {
- types: ['restaurants','food trucks']
-};
-var autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'),acOptions);
-autocomplete.bindTo('bounds',map);
-var infoWindow = new google.maps.InfoWindow();
-var marker = new google.maps.Marker({
- map: map
-});
+        var myOptions = {
+          zoom : 16,
+          center : userLatLng,
+          mapTypeId : google.maps.MapTypeId.ROADMAP
+        };
+        // Draw the map
+        var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
+        // Place the marker
+        new google.maps.Marker({
+          map: mapObject,
+          position: userLatLng
+        });
+        // Draw a circle around the user position to have an idea of the current localization accuracy
+        var circle = new google.maps.Circle({
+          center: userLatLng,
+          radius: position.coords.accuracy,
+          map: mapObject,
+          fillColor: '#0000FF',
+          fillOpacity: 0.5,
+          strokeColor: '#0000FF',
+          strokeOpacity: 1.0
+        });
+        mapObject.fitBounds(circle.getBounds());
+      }
 
-google.maps.event.addListener(autocomplete, 'place_changed', function() {
- infoWindow.close();
- var place = autocomplete.getPlace();
- if (place.geometry.viewport) {
-   map.fitBounds(place.geometry.viewport);
- } else {
-   map.setCenter(place.geometry.location);
-   map.setZoom(17);
- }
- marker.setPosition(place.geometry.location);
- infoWindow.setContent('<div><strong>' + place.name + '</strong><br>');
- infoWindow.open(map, marker);
- google.maps.event.addListener(marker,'click',function(e){
+      function geolocationError(positionError) {
+        document.getElementById("error").innerHTML += "Error: " + positionError.message + "<br />";
+      }
 
-   infoWindow.open(map, marker);
+      function geolocateUser() {
+        // If the browser supports the Geolocation API
+        if (navigator.geolocation)
+        {
+          var positionOptions = {
+            enableHighAccuracy: true,
+            timeout: 10 * 1000 // 10 seconds
+          };
+          navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError, positionOptions);
+        }
+        else
+          document.getElementById("error").innerHTML += "Your browser doesn't support the Geolocation API";
+      }
 
-  });
- });
+      window.onload = geolocateUser;
