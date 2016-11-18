b77281df00ce7ea075d2faf240395146b8d50d64
Christine van den Vlekkert <xine.vdv@me.com>
2016-11-17 18:28:46 -0800

    adding changes

--- assets/javascript/dbvisitor.js
+++ assets/javascript/dbvisitor.js
@@ -7,7 +7,13 @@ var config = {
   };
   firebase.initializeApp(config);
 
-  var firebase = new Firebase('https://realtime-eaterie-1478752187177.firebaseio.com');
+  var analytics = new Firebase ('https://realtime-eaterie-1478752187177.firebaseio.com');
+  var activeUser = analytics.child('activeUser');
+    activeVisitors.push({
+      path: window.location.pathname,
+      arrivedAt: Firebase.ServerValue.TIMESTAMP,
+      userAgent: navigator.userAgent
+    });
 
       /**
       * Data object to be written to Firebase.
@@ -19,171 +25,85 @@ var config = {
         lng: null
       };
 
-      function makeInfoBox(controlDiv, map) {
-        // Set CSS for the control border.
-        var controlUI = document.createElement('div');
-        controlUI.style.boxShadow = 'rgba(0, 0, 0, 0.298039) 0px 1px 4px -1px';
-        controlUI.style.backgroundColor = '#fff';
-        controlUI.style.border = '2px solid #fff';
-        controlUI.style.borderRadius = '2px';
-        controlUI.style.marginBottom = '22px';
-        controlUI.style.marginTop = '10px';
-        controlUI.style.textAlign = 'center';
-        controlDiv.appendChild(controlUI);
-
-        // Set CSS for the control interior.
-        var controlText = document.createElement('div');
-        controlText.style.color = 'rgb(25,25,25)';
-        controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
-        controlText.style.fontSize = '100%';
-        controlText.style.padding = '6px';
-        controlText.textContent = 'The map shows all clicks made in the last 10 minutes.';
-        controlUI.appendChild(controlText);
-      }
-
-      /**
-      * Starting point for running the program. Authenticates the user.
-      * @param {function()} onAuthSuccess - Called when authentication succeeds.
-      */
-      function initAuthentication(onAuthSuccess) {
-        firebase.authAnonymously(function(error, authData) {
-          if (error) {
-            console.log('Login Failed!', error);
-          } else {
-            data.sender = authData.uid;
-            onAuthSuccess();
-          }
-        }, {remember: 'sessionOnly'});  // Users will get a new id for every session.
-      }
-
-      /**
-       * Creates a map object with a click listener and a heatmap.
-       */
-      function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 0, lng: 0},
-          zoom: 3,
-          styles: [{
-            featureType: 'poi',
-            stylers: [{ visibility: 'off' }]  // Turn off POI.
-          },
-          {
-            featureType: 'transit.station',
-            stylers: [{ visibility: 'off' }]  // Turn off bus, train stations etc.
-          }],
-          disableDoubleClickZoom: true,
-          streetViewControl: false,
+      var totalVisitor = analytics.child('totalVisitor');
+        totalVisitor.once('value', function(snapshot) {
+          totalVisitor.set(snapshot.val()+1);
         });
 
-        // Create the DIV to hold the control and call the makeInfoBox() constructor
-        // passing in this DIV.
-        var infoBoxDiv = document.createElement('div');
-        makeInfoBox(infoBoxDiv, map);
-        map.controls[google.maps.ControlPosition.TOP_CENTER].push(infoBoxDiv);
+        var totalVisitors = analytics.child('totalVisitors');
+          totalVisitors.transaction(function (currentData) {
+            return currentData + 1;
+});
 
-        // Listen for clicks and add the location of the click to firebase.
-        map.addListener('click', function(e) {
-          data.lat = e.latLng.lat();
-          data.lng = e.latLng.lng();
-          addToFirebase(data);
-        });
+      var visitor = {
+          path: window.location.pathname,
+          arrivedAt: Firebase.ServerValue.TIMESTAMP,
+          userAgent: navigator.userAgent
+};
 
-        // Create a heatmap.
-        var heatmap = new google.maps.visualization.HeatmapLayer({
-          data: [],
-          map: map,
-          radius: 16
-        });
+    var activeVisitorRef = activeVisitors.push(visitor, function () {
+        activeVisitors.child(visitorId).once('value', function (snapshot) {
+        visitor.arrivedAt = snapshot.child('arrivedAt').val();
+    var pastVisitors = analytics.child('pastVisitors');
+      visitor.leftAt = Firebase.ServerValue.TIMESTAMP;
+      pastVisitors.child(visitorId).onDisconnect().set(visitor);
+    });
+});
 
-        initAuthentication(initFirebase.bind(undefined, heatmap));
-      }
+    var visitorId = activeVisitorRef.name();
 
-      /**
-       * Set up a Firebase with deletion on clicks older than expirySeconds
-       * @param {!google.maps.visualization.HeatmapLayer} heatmap The heatmap to
-       * which points are added from Firebase.
-       */
-      function initFirebase(heatmap) {
+      activeVisitorRef.onDisconnect().remove();
 
-        // 10 minutes before current time.
-        var startTime = new Date().getTime() - (60 * 10 * 1000);
+      $(document).on('ready', function () {
+    var $totalVisitors = $('#total-visitors');
+      analytics.child('totalVisitors').on('value', function (snapshot) {
+        $totalVisitors.text(snapshot.val());
+  });
+});
+    var $activeVisitors = $('#active-visitors');
+    var activeVisitors = analytics.child('activeVisitors');
+      activeVisitors.on('child_added', function (snapshot) {
+        var n = snapshot.name();
+        var v = snapshot.val();
+        $activeVisitors.prepend(
+          '<li id="active-visitor' + n + '">' + n + ':' +
+          '<ul>' +
+          '<li>Arrived: ' + new Date(v.arrivedAt) + '</li>' +
+          '<li>Path: ' + v.path + '</li>' +
+          '<li>User Agent: ' + v.userAgent + '</li>' +
+          '</ul>' +
+          '</li>'
+);
+});
+    var pastVisitors = analytics.child('pastVisitors');
+      pastVisitors.on('child_added', function (snapshot) {
+    var n = snapshot.name();
+    var v = snapshot.val();
+    $pastVisitors.prepend(
+      '<li id="past-visitor' + n + '">' + n + ':' +
+      '<ul>' +
+      '<li>Arrived: ' + new Date(v.arrivedAt) + '</li>' +
+      '<li>Left: ' + new Date(v.leftAt) + '</li>' +
+      '<li>Spent: ' + ((v.leftAt - v.arrivedAt) / 1000) + ' Seconds </li>' +
+      '<li>Path: ' + v.path + '</li>' +
+      '<li>User Agent: ' + v.userAgent + '</li>' +
+      '</ul>' +
+      '</li>'
+);
+});
 
-        // Reference to the clicks in Firebase.
-        var clicks = firebase.child('clicks');
+    activeVisitors.on('child_removed', function (snapshot) {
+      $('#active-visitor' + snapshot.name()).remove();
+    });
 
-        // Listener for when a click is added.
-        clicks.orderByChild('timestamp').startAt(startTime).on('child_added',
-          function(snapshot) {
+    var pastVisitors = analytics.child('pastVisitors').endAt().limit(3);
+      pastVisitors.on('child_added', function (snapshot) {
+ // ...
+});
 
-            // Get that click from firebase.
-            var newPosition = snapshot.val();
-            var point = new google.maps.LatLng(newPosition.lat, newPosition.lng);
-            var elapsed = new Date().getTime() - newPosition.timestamp;
+      pastVisitors.on('child_removed', function (snapshot) {
+    $('#past-visitor' + snapshot.name()).remove();
+  });
 
-            // Add the point to  the heatmap.
-            heatmap.getData().push(point);
 
-            // Requests entries older than expiry time (10 minutes).
-            var expirySeconds = Math.max(60 * 10 * 1000 - elapsed, 0);
-            // Set client timeout to remove the point after a certain time.
-            window.setTimeout(function() {
-              // Delete the old point from the database.
-              snapshot.ref().remove();
-            }, expirySeconds);
-          }
-        );
-
-        // Remove old data from the heatmap when a point is removed from firebase.
-        clicks.on('child_removed', function(snapshot, prevChildKey) {
-          var heatmapData = heatmap.getData();
-          var i = 0;
-          while (snapshot.val().lat != heatmapData.getAt(i).lat()
-            || snapshot.val().lng != heatmapData.getAt(i).lng()) {
-            i++;
-          }
-          heatmapData.removeAt(i);
-        });
-      }
-
-      /**
-       * Updates the last_message/ path with the current timestamp.
-       * @param {function(Date)} addClick After the last message timestamp has been updated,
-       *     this function is called with the current timestamp to add the
-       *     click to the firebase.
-       */
-      function getTimestamp(addClick) {
-        // Reference to location for saving the last click time.
-        var ref = firebase.child('last_message/' + data.sender);
-
-        ref.onDisconnect().remove();  // Delete reference from firebase on disconnect.
-
-        // Set value to timestamp.
-        ref.set(Firebase.ServerValue.TIMESTAMP, function(err) {
-          if (err) {  // Write to last message was unsuccessful.
-            console.log(err);
-          } else {  // Write to last message was successful.
-            ref.once('value', function(snap) {
-              addClick(snap.val());  // Add click with same timestamp.
-            }, function(err) {
-              console.warn(err);
-            });
-          }
-        });
-      }
-
-      /**
-       * Adds a click to firebase.
-       * @param {Object} data The data to be added to firebase.
-       *     It contains the lat, lng, sender and timestamp.
-       */
-      function addToFirebase(data) {
-        getTimestamp(function(timestamp) {
-          // Add the new timestamp to the record data.
-          data.timestamp = timestamp;
-          var ref = firebase.child('clicks').push(data, function(err) {
-            if (err) {  // Data was not written to firebase.
-              console.warn(err);
-            }
-          });
-        });
-      }
+      
